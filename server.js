ca0c798bd08dc31f2b7583fc6b06cbed72b86061
OichanST <56617025+OichanST@users.noreply.github.com>
2020-11-12 14:41:52 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const PORT = process.env.PORT || 8080;
 
-const SF_INSTANCE_URL = "3MVG9rnryk9FxFMXMi93yAP6uKBjb3YsMBzAqjdXCT9YR.4Trha3ANDntg4SLcPvAtFj9DL8TmSCf42lUmaU.";
-const REDIRECT_URI = "";
+const SF_INSTANCE_URL = "https://sato-global-jp--2019USE.my.salesforce.com";
+const SF_CLIENT_ID = "3MVG9rnryk9FxFMXMi93yAP6uKBjb3YsMBzAqjdXCT9YR.4Trha3ANDntg4SLcPvAtFj9DL8TmSCf42lUmaU.";
+const SF_REDIRECT_URI = "https://backlogconnect.herokuapp.com/token";
 
 const express = require("express");
 const parser = require("body-parser");
@@ -21,15 +22,26 @@ console.log("server start PORT=" + PORT);
 let bodyData = null;
 
 // 認証をする
-app.get('/auth', function(req, res){
+app.use('/auth', function(req, res){
 	res.set('Content-Type', 'text/html');
-	const html = "<script>location.href = 'http://google.com';</script>";
+	const html = 
+	"<form id='f' name='f' method='POST' action='" + SF_INSTANCE_URL + "/services/oauth2/authorize'>" +
+	"	<input type='hidden' name='response_type' value='code'/>" +
+	"	<input type='hidden' name='client_id' value='" + SF_CLIENT_ID + "'/>" + 
+	"	<input type='hidden' name='redirect_uri' value='" + SF_REDIRECT_URI + "'/>" + 
+	"</form>" +
+	"<script>" +
+	"document.forms.f.submit();" +
+	"</script>";
 	res.send(Buffer.from(html));
-	res.redirect(307, SF_INSTANCE_URL + "/oauth2/authorize");
+});
+
+app.use('/token', function(req, res){
+	console.log(req.query);
 });
 
 // BacklogのWebhookからのコール
-app.post('/webhook', function(req, res){
+app.use('/webhook', function(req, res){
 	console.log(req.url);
 	console.log(req.query);
 	console.log(req.body);
