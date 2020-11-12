5d446ecf3490715c59ebbd54c9eb3b229e76b52e
OichanST <56617025+OichanST@users.noreply.github.com>
2020-11-12 13:09:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ const http = require("http");
 const PORT = process.env.PORT || 8080;
 
 http.createServer(function(req, res){
-	/*
+	console.log("--- Request Listen ---");
 	console.log(req);
 	console.log(res);
-	*/
-	console.log("request listen");
-	res.end();
 }).listen(PORT);
 
 console.log("server start PORT=" + PORT);
